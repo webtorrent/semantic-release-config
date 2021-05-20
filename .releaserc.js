3c2b5f781715d1a3c0e1dc0e41ff81e74a9a3398
Diego Rodríguez Baquero <diegorbaquero@gmail.com>
2021-05-20 11:47:21 -0500

    Create .releaserc.js

---
+++ .releaserc.js
@@ -0,0 +1,46 @@
+// Extra types that generate a patch releases
+const typesForPatch = ['docs', 'refactor', 'perf']
+
+module.exports = {
+    branches: [
+        'master'
+    ],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    ...typesForPatch.map((type) => ({
+                        type,
+                        release: 'patch'
+                    }))
+                ],
+                parserOpts: {
+                    noteKeywords: []
+                }
+            }
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                parserOpts: {
+                    noteKeywords: []
+                }
+            }
+        ],
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'package.json',
+                    'package-lock.json',
+                    'CHANGELOG.md'
+                ],
+                message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}'
+            }
+        ],
+        '@semantic-release/github'
+    ]
+}
